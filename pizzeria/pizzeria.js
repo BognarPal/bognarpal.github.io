6e43a8db4b2dfdf9fba942cdacd7b34524497db1
Bognár Pál <bognar.pal@outlook.hu>
2021-07-19 12:31:27 +0200

    Interface-k tervezése

---
+++ pizzeria/pizzeria.js
@@ -0,0 +1,118 @@
+function loadSourceCode(htmlElement, url, functionSignature) {
+    url = 'https://raw.githubusercontent.com/BognarPal/' + url;
+    var xhrSourceCode = new XMLHttpRequest();
+    xhrSourceCode.addEventListener('readystatechange', function () {
+        if (this.readyState === 4) {
+            if (functionSignature) {    
+                var lines = this.responseText.split('\n');
+                var sourceCode = '';
+                var bracesCount = undefined;
+                for (line of lines) {                    
+                    if (line.indexOf(functionSignature) !== -1 || sourceCode) {
+                        sourceCode += line + '\n';
+                        bracesCountChange = (line.split('{').length - 1) - (line.split('}').length - 1);
+                        if (bracesCountChange !== 0) {
+                            if (bracesCount == undefined ) {
+                                bracesCount = bracesCountChange;
+                            } else {
+                                bracesCount += bracesCountChange;
+                                if (bracesCount === 0)
+                                    break;
+                            }
+                        }
+                    }
+                };
+                htmlElement.innerHTML = hljs.highlightAuto(sourceCode).value;
+            } else {                
+                htmlElement.innerHTML = hljs.highlightAuto(this.responseText).value;
+            }
+            location.href = location.href;
+        }
+    });
+
+    xhrSourceCode.open('GET', url);
+    xhrSourceCode.overrideMimeType('text/plain; charset=iso-8859-1');
+    xhrSourceCode.send();
+}  
+
+function loadContent(page) {    
+    var gotoToId = '#';
+    if (page.indexOf('#') !== -1 ) {
+        gotoToId = page.substr(page.indexOf('#'), page.length);
+        page = page.substr(0, page.indexOf('#'));
+    }
+
+    sourceCodeCount = 0;
+    var xhrContent = new XMLHttpRequest();
+    xhrContent.addEventListener('readystatechange', function () {
+        if (this.readyState === 4) {
+            document.querySelector('#content').innerHTML = this.responseText;
+            hljs.highlightAll();            
+            history.pushState(null, '', '/pizzeria/main.html?page=' + page);
+            location.href = gotoToId;
+
+            document.querySelectorAll('pre code[data-url]').forEach( element => {
+                sourceCodeCount++;
+                if (element.attributes['data-functionSignature']) {
+                    loadSourceCode(element, element.attributes['data-url'].value, element.attributes['data-functionSignature'].value)
+                } else {
+                    loadSourceCode(element, element.attributes['data-url'].value)
+                }
+            });
+            
+            var menuElement = document.querySelector('#sidebar p[data-page="' + page + '"]');
+            if (gotoToId !='#') {
+                menuElement = document.querySelector('#sidebar p[data-page="' + page + gotoToId + '"]')
+            }
+            document.querySelectorAll('#sidebar p[data-page]').forEach( element => {
+                element.style.fontWeight = "normal";
+                element.style.color = "#eee";
+            });
+            menuElement.style.fontWeight = "bold";
+            menuElement.style.color = "#6ee";
+
+            document.querySelectorAll('div#sidebar p[class~="small"]').forEach(element => {
+                if (element.attributes['data-page'].value.indexOf(page) === -1) {
+                    element.style.display = 'none';
+                } else {
+                    element.style.display = null;
+                }
+            });
+        }
+    });    
+
+    var selectedMenuElement = document.querySelector('#sidebar p[data-page='+page+']');
+    if (!selectedMenuElement) {
+        page = 'feladatDefinicio';
+        selectedMenuElement = document.querySelector('#sidebar p[data-page='+page+']');
+    }
+    document.title = 'Pizzéria: ' + selectedMenuElement.attributes['data-title'].value;
+    xhrContent.open('GET', page + '.html');
+    xhrContent.send();
+}        
+
+var xhrSideBar = new XMLHttpRequest();
+xhrSideBar.addEventListener('readystatechange', function () {
+    if (this.readyState === 4) {
+        document.querySelector('#sidebar').innerHTML = this.responseText;
+
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const params = Object.fromEntries(urlSearchParams.entries());
+        var page = params['page'];
+        if (!page) {
+            page = 'feladatDefinicio'
+        }    
+    
+        loadContent(page);           
+        
+        document.getElementById('sidebar').addEventListener('click', (event) => {
+            loadContent(event.target.attributes['data-page'].value);
+        });
+    }
+});
+
+xhrSideBar.open('GET', 'sidebar.html');
+xhrSideBar.send();
+
+
+
